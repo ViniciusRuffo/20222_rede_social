1f4d988eb4fc97f6eaa03ccc186147e7fba749e0
Vinicius Ruffo Viviani - 821156267 <821156267@ulife.com.br>
2022-09-12 21:37:50 -0300

    feat: adicionado metodos get e post

--- rede/index.js
+++ rede/index.js
@@ -1,4 +1,28 @@
 const express = require('express');
 const app = express();
+app.use(express.json())
+
+let contador = 0;
+
+//Base de dados em memória volátil
+const usuarios = {
+}
+//GET base de dados usuários
+app.get('/rede', (req, res) => {
+    console.log('Requisição GET..')
+    res.send(usuarios)
+})
+app.post('/rede', (req, res) => {
+    contador++
+    const nome = req.body.nome
+    const email = req.body.email
+    usuarios[contador] = {
+        id: contador,
+        nome: nome,
+        email: email
+    }
+    res.status(201).end()
+})
+
 
 app.listen(5000, () => console.log('Escutando na porta 5000'))
